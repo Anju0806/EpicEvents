9b3104397e636cef6c7719aef7d5485c1020e515
anju <anjualfino@gmail.com>
2023-09-03 13:06:36 +0800

    adding hamburger icon

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,88 +1,130 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-//import { Flex, Box, Text, Button, Spacer, Heading } from '@chakra-ui/react';
-//import { Flex, Box, Text, Button, Spacer, Heading, IconButton } from '@chakra-ui/react'; // Import the Icon component
-import { Flex, Box, Text, Button, Spacer, Heading } from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
-
+import { Flex, Box, Text, Heading, useBreakpointValue } from '@chakra-ui/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'; // Import the cross icon
 import Auth from '../../utils/auth';
 
 const Navbar = () => {
   const logout = (event) => {
     event.preventDefault();
-    Auth.logout();                            
+    Auth.logout();
   };
-/*   const [showMenu, setShowMenu] = useState(false);
 
+  // State to track whether the menu is open or closed
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Function to toggle the menu's open/close state
   const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  }; */
+    setIsMenuOpen(!isMenuOpen);
+  };
 
+  // Use breakpoint value to determine if it's a small screen
+  const isSmallScreen = useBreakpointValue({ base: true, sm: true, md: false, lg: false, xl: false });
 
   return (
-    <Flex bg="#38714B" p={0} color="white" minWidth='max-content' alignItems='center' gap='2' >
-      <Box p='2'>
+    <Flex bg="#38714B" p={0} color="white" alignItems="center" justifyContent="space-between">
+      <Box p="2">
         <Link to="/">
-          <Heading size='md' >Epic Events</Heading>
+          <Heading size="md">Epic Events</Heading>
         </Link>
       </Box>
-      <Spacer />
-      <Box>
-{/* <div><HamburgerIcon /></div> */}
-      
-
-       {/*  <Button
-          display={['block', 'block', 'none']}
-          onClick={toggleMenu} >
-          {showMenu ? <CloseIcon /> : <HamburgerIcon />} 
-        </Button>
- */}
-
-      </Box>
-      <Flex
-        direction={['column', 'row']} // Column on small screens, row on larger screens
-        display={['none', 'flex', 'flex']} // Hide on small screens, show on larger screens
-        alignItems={['center', 'center', 'flex-end']} // Adjust alignment
-      >
-        <Text >
-          <Link  to="/contact">
-            Contact
-          </Link>
-        </Text>
-        {Auth.loggedIn() ? (
-          <Flex >
-            <Text >
-              <Link  m='2' to="/me">
-                {Auth.getProfile().data.username}'s profile
-              </Link>
-            </Text>
-            <Text>
-              <Link color='teal.500' to="/addEvent">
-                Create Event
-              </Link>
-            </Text>
-            <Text >
-              <Link color='teal.500' to="/" onClick={logout}>
-                Logout
-              </Link>
-            </Text>
-
-          </Flex>
-        ) : (
-          <Flex>
-            <Text>
-              <Link color='teal.500' to="/login">
-                Login
-              </Link>
-            </Text>
-            <Text>
-              <Link color='teal.500' to="/signup">
-                Signup
-              </Link>
-            </Text>
-          </Flex>
-        )}
-      </Flex>
+      {/* Display the hamburger menu only on small screens */}
+      {isSmallScreen && (
+        <Box display={['block', 'block', 'none']} p="2">
+          {/* Toggle icon (hamburger or cross) */}
+          <FontAwesomeIcon
+            icon={isMenuOpen ? faTimes : faBars}
+            style={{ cursor: 'pointer' }}
+            size="lg"
+            onClick={toggleMenu} // Toggle the menu when clicked
+          />
+        </Box>
+      )}
+      {/* Navigation links */}
+      {isSmallScreen ? (
+        <Box
+          display={isMenuOpen ? 'block' : 'none'}
+          bg="#38714B"
+          color="white"
+          position="absolute"
+          top="58px"
+          left="0"
+          right="0"
+          zIndex="1"
+          textAlign="center"
+        >
+          <Text p="2">
+            <Link to="/contact">Contact</Link>
+          </Text>
+          {Auth.loggedIn() ? (
+            <>
+              <Text p="2">
+                <Link to="/me">{Auth.getProfile().data.username}'s profile</Link>
+              </Text>
+              <Text p="2">
+                <Link color="teal.500" to="/addEvent">
+                  Create Event
+                </Link>
+              </Text>
+              <Text p="2">
+                <Link color="teal.500" to="/" onClick={logout}>
+                  Logout
+                </Link>
+              </Text>
+            </>
+          ) : (
+            <>
+              <Text p="2">
+                <Link color="teal.500" to="/login">
+                  Login
+                </Link>
+              </Text>
+              <Text p="2">
+                <Link color="teal.500" to="/signup">
+                  Signup
+                </Link>
+              </Text>
+            </>
+          )}
+        </Box>
+      ) : (
+        <Flex alignItems="center">
+          <Text p="2">
+            <Link to="/contact">Contact</Link>
+          </Text>
+          {Auth.loggedIn() ? (
+            <Flex>
+              <Text p="2">
+                <Link to="/me">{Auth.getProfile().data.username}'s profile</Link>
+              </Text>
+              <Text p="2">
+                <Link color="teal.500" to="/addEvent">
+                  Create Event
+                </Link>
+              </Text>
+              <Text p="2">
+                <Link color="teal.500" to="/" onClick={logout}>
+                  Logout
+                </Link>
+              </Text>
+            </Flex>
+          ) : (
+            <Flex>
+              <Text p="2">
+                <Link color="teal.500" to="/login">
+                  Login
+                </Link>
+              </Text>
+              <Text p="2">
+                <Link color="teal.500" to="/signup">
+                  Signup
+                </Link>
+              </Text>
+            </Flex>
+          )}
+        </Flex>
+      )}
     </Flex>
   );
 };
